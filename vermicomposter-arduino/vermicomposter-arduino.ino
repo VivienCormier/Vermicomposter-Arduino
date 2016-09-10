6cb9892b37c8ea98bb1dfa21f149bdcfd2ea4fc4
Xcode Server <xcodeserver@Xcode-Server.local>
2016-09-10 18:48:21 +0200

    [F] Fix bugs + add second temperature sensor

--- vermicomposter-arduino/vermicomposter-arduino.ino
+++ vermicomposter-arduino/vermicomposter-arduino.ino
@@ -7,7 +7,7 @@
 #define COND_PWD_PIN 10   // Power pin of the condenstion sensor
 
 // Humidity box leve 1
-#define HUMD1_DATA_PIN 6   // Data pin of the humidity sensor
+#define HUMD1_DATA_PIN A0   // Data pin of the humidity sensor
 #define HUMD1_PWD_PIN 7   // Power pin of the humidity sensor
 
 // Top Fan params
@@ -25,15 +25,15 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature. 
 DallasTemperature sensors(&oneWire);
 
+bool forceFirstLoop = true;
 
 unsigned long previousMillis = 0; // last time update
-long interval = 15 * 60 * 1000; // interval at which to do something (milliseconds)
+unsigned long interval = 15 * 60 * 1000L; // interval at which to do something (milliseconds)
 
 void setup() {
   Serial.begin(9600);
   pinMode(COND_DATA_PIN, INPUT);
   pinMode(COND_PWD_PIN, OUTPUT);
-  pinMode(HUMD1_DATA_PIN, INPUT);
   pinMode(HUMD1_PWD_PIN, OUTPUT);
   pinMode(FANPIN, OUTPUT);
   sensors.begin();
@@ -43,12 +43,13 @@ void loop() {
   
   unsigned long currentMillis = millis();
 
-  if(currentMillis - previousMillis > interval || currentMillis == 0) {
+  if(currentMillis - previousMillis > interval || forceFirstLoop) {
      previousMillis = currentMillis;  
+
+     forceFirstLoop = false;
 
     // Check the condensation and turn on the fan if needed
     bool condensationIsDetected = condensationDetected();
-    activateFan(condensationDetected);
 
     // Activate temperature sensor
     sensors.requestTemperatures();
@@ -58,6 +59,12 @@ void loop() {
 
     // Humidity level 1
     float humdLevel1 = humidityLevel1();
+
+    // Temperature level 2
+    float tempLevel2 = temperatureLevel2();
+
+    activateFanTopBox(condensationIsDetected || tempLevel2 > 26.0);
+    activateFanLevel1(tempLevel1>26.0);
 
     Serial.print("Condensation detected : ");
     if (condensationIsDetected) {
@@ -69,17 +76,26 @@ void loop() {
     Serial.print(tempLevel1);
     Serial.print("°C");
     Serial.print(" | Humd level 1 : ");
-    Serial.println(humdLevel1);
-    
+    Serial.print(humdLevel1);
+    Serial.print(" | Temp level 2 : ");
+    Serial.println(tempLevel2);
   }
   
 }
 
-void activateFan(boolean activate) {
+void activateFanTopBox(boolean activate) {
   if (activate) {
     analogWrite(FANPIN, 255);
   } else {
     analogWrite(FANPIN, 0);
+  }
+}
+
+void activateFanLevel1(boolean activate) {
+  if (activate) {
+    analogWrite(FAN1_PWD_PIN, 255);
+  } else {
+    analogWrite(FAN1_PWD_PIN, 0);
   }
 }
 
@@ -93,6 +109,10 @@ bool condensationDetected() {
 }
 
 float temperatureLevel1() {
+   return sensors.getTempCByIndex(1);
+}
+
+float temperatureLevel2() {
    return sensors.getTempCByIndex(0);
 }
 
@@ -101,6 +121,8 @@ float humidityLevel1() {
   analogWrite(HUMD1_PWD_PIN, 255);
   delay(1000);
   float humidity = analogRead(HUMD1_DATA_PIN);
+//  humidity = 100 * humidity / 18;
+//  delay(2000);
   analogWrite(HUMD1_PWD_PIN, 0);
   return humidity;
 }
